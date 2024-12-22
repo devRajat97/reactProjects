fa4bc211ed5b792107f230640ce1925a242cc2a7
rajat dev <rajatdev971@gmail.com>
2024-12-22 19:50:59 +0530

    add files

---
+++ src/components/body.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const body = () => {
+  return (
+    <div>
+      <h1>Body</h1>
+    </div>
+  )
+}
+
+export default body
---
+++ src/components/form.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const form = () => {
+  return (
+    <div>
+      <h1>from</h1>
+    </div>
+  )
+}
+
+export default form
